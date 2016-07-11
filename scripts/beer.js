7b9fb27c7cf9bda5fa603d622760381f820b6d55
Daniel Douangkesone <karateporkchop@gmail.com>
2016-07-11 18:49:23 -0500

    conused script

--- scripts/beer.js
+++ scripts/beer.js
@@ -1,10 +1,40 @@
-module.export = function(robot) {
-  var beers = 0;
+// module.exports = function(robot) {
+//   robot.respond(/take down (\d+)/i, function(msg) {
+//     msg.send('WEEEE!');
+//   })
+// };
 
-  robot.respond(/take down (\d+)/i, takeDownBeers);
 
-  function takeDownBeers(msg) {
-  console.log('hit');
+
+
+module.exports = function(robot) {
+
+
+  robot.hear(/wee/i, takeDownBeers);
+  robot.hear(/check/i, checkBeers);
+  var beers = 99;
+
+  function takeDownBeers(msg){
+    var takeDownCount = beers - 1;
+    var message;
+
+if(beers <= 0){
+  message = "No more bottles of beer on the wall, no more bottles of beer.Go to the store and buy some more, 99 bottles of beer on the wall.";
+}
+else if(beers >= 1){
+  message = beers + " bottles of beer on the wall! " + beers + " Bottles of beer! Take one down and pass it around " + takeDownCount + ' bottles of beer on the wall!';
+  var beer = beer - 1;
+
+}
+else{
+  message = "hit! else";
+}
+msg.send(message);
+
   }
-   
+
+};
+
+function checkBeers(msg){
+  msg.send('You have ' + beers + 'left!');
 }
