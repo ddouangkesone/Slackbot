9df63e4574d2fb0e4bfa6fbf8f63a8b49dc42c92
Daniel Douangkesone <karateporkchop@gmail.com>
2016-07-11 19:32:51 -0500

    randomizing feature added

--- scripts/kat.js
+++ scripts/kat.js
@@ -3,7 +3,9 @@ module.exports = function(robot){
   robot.hear(/kitten/i, meow);
 
   function meow(msg){
-    var catImage = 'https://pbs.twimg.com/profile_images/562466745340817408/_nIu8KHX.jpeg';
+    robot.http("http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=kitten").get(function(err, res, body){
+      console.log(body);
+    });
 
     msg.send(catImage);
 
